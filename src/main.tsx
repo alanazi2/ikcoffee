b5847d4554bd6337fcd39675a391bf7c774c3bbc
RASHID AL ANAZI <alanazirashid4@gmail.com>
2025-05-05 12:00:00 +0300

    Fix: remove duplicate App import

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,14 @@
-
 // src/main.tsx
-import React from 'react';
+
 import ReactDOM from 'react-dom/client';
-import './i18n'; // فوق App.tsx
+import { StrictMode } from 'react';
 import App from './App.tsx';
+import './i18n';
 import './App.css';
+import './index.css'; // إذا كنت تستخدم index.css
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
-
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
-
+  </StrictMode>
+);
